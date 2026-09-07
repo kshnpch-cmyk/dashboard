834fd6a0ea1631c4f331c4bb3ff03433f955f8a4
kshnpch-cmyk <kshnpch@gmail.com>
2026-09-07 11:23:55 +0900

    Update screenshot.py

--- screenshot.py
+++ screenshot.py
@@ -7,7 +7,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
-WEBHOOK_URL = "https://script.google.com/macros/s/AKfycbw-vPBcUbAjxHdP492e3z_JR1M74WZIp2q-48v6idKD3XjrBu5C_Fns00ybhzf3d-aa/exec"
+WEBHOOK_URL = "https://script.google.com/macros/s/AKfycbzdbqLJJIqQ8cXxMgyadZXwERo21zwJaTBeVtQuvyqSmXpyHscOJNWJ_76UR2J_Rm4g/exec"
 
 # 1. 한국 시간(KST) 및 항상 다음날(오늘 + 1일) 날짜 자동 계산
 KST = timezone(timedelta(hours=9))
